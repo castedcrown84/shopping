2c1a1939e473bc0d42526697542302649152aa5d
Roberto Monplaisir <castedcrown84@gmail.com>
2023-09-08 16:19:55 -0400

    1st commit. added and styled Navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import NavGuide from './components/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+    <NavGuide/>
+    <div className="content">
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
